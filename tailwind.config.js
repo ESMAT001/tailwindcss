ed3319f719e0b8301b7c5d07294bc380b725cee3
ESMAT001 <esmat10.niazi@gmail.com>
2021-01-30 09:54:33 +0430

    practice

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,37 +2,10 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      fontFamily:{
-        new:[
-          "'Nunito'",
-          'ui-sans-serif',
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          '"Segoe UI"',
-          'Roboto',
-          '"Helvetica Neue"',
-          'Arial',
-          '"Noto Sans"',
-          'sans-serif',
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-          '"Noto Color Emoji"'
-        ]
-      }
-      ,
-      transitionProperty: {
-        'height': 'height'
-        // 'spacing': 'margin, padding',
-       }
-    },
+    extend: {},
   },
   variants: {
-    extend: {
-      transitionProperty: ['hover', 'focus']
-    },
+    extend: {},
   },
   plugins: [],
 }
